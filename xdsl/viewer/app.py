75601a3c1b5ae0417645b07e6c686cef6ebac6d7
xhu6 <143360206+xhu6@users.noreply.github.com>
2026-02-19 17:37:27 +0000

    viewer: refactor

--- xdsl/viewer/app.py
+++ xdsl/viewer/app.py
@@ -1,10 +1,13 @@
 import argparse
-from enum import Enum, auto
+from bisect import bisect_right
+from dataclasses import dataclass
 
 
-class Graph:
+class Lines:
     """
-    Directed graph, using adjacency list. Nodes have ordering.
+    Lines with jumps.
+
+    Lines are strings that do not have any newlines or control characters.
     """
 
     def __init__(self) -> None:
@@ -12,127 +15,128 @@ class Graph:
         self.adj_t: list[set[int]] = []
         self.nodes: list[str] = []
 
-    def add_node(self, data: str) -> int:
-        new_id = self.num_of_nodes()
+    def add_line(self, line: str) -> int:
+        new_id = len(self)
 
         self.adj.append(set())
         self.adj_t.append(set())
-        self.nodes.append(data)
+        self.nodes.append(line)
 
         return new_id
 
-    def add_edge(self, start_id: int, end_id: int) -> None:
+    def add_jump(self, start_id: int, end_id: int) -> None:
         self.adj[start_id].add(end_id)
         self.adj_t[end_id].add(start_id)
 
-    def num_of_nodes(self) -> int:
+    def __len__(self) -> int:
         return len(self.adj)
 
 
-class ArrowEnum(Enum):
-    Normal = auto()
-    Outgoing = auto()
-    Incoming = auto()
+@dataclass
+class Jmp:
+    start: int
+    end: int
+    reversed: bool
 
 
-def format_slots(
-    slots: list[int | None], arrow: ArrowEnum, indexes: list[int] = []
-) -> str:
-    out: list[str] = []
+class Margin:
+    def __init__(self, lines: Lines) -> None:
+        self.columns: list[list[Jmp]] = []
+        self.lines = lines
 
-    discovered = False
+        for line_no in range(len(lines)):
+            for dest in lines.adj[line_no]:
+                # Will be handled by adj_t case
+                if dest < line_no:
+                    continue
 
-    for i, slot in enumerate(slots):
-        if i in indexes:
-            discovered = True
-            out.append("+")
+                jmp = Jmp(line_no, dest, False)
+                self._insert(jmp)
 
-        elif discovered:
-            out.append("-")
+            for dest in lines.adj_t[line_no]:
+                if dest <= line_no:
+                    continue
 
-        elif slot is None:
-            out.append(" ")
+                jmp = Jmp(line_no, dest, True)
+                self._insert(jmp)
 
+    def _insert(self, jmp: Jmp) -> None:
+        for col in self.columns:
+            if len(col) == 0 or col[-1].end <= jmp.start:
+                col.append(jmp)
+                break
         else:
-            out.append("|")
+            self.columns.append([jmp])
 
-    if not discovered:
-        out.append(" ")
+    def display_outgoing(self, line_no: int, line_width: int = 8) -> str:
+        line_width -= 2
+        out: list[str] = [" "] * (line_width - len(self.columns))
+        horizontal = False
 
-    elif arrow == ArrowEnum.Outgoing:
-        out.append("<")
+        for col in self.columns[:line_width]:
+            # bisect_right gets line_no < x.start
+            # subtract 1 to get x.start <= line_no (our lower bound)
+            index = bisect_right(col, line_no, key=lambda x: x.start) - 1
 
-    elif arrow == ArrowEnum.Incoming:
-        out.append(">")
+            if index < 0 or len(col) <= index:
+                out.append(" " if not horizontal else "-")
+                continue
 
-    else:
-        out.append(" ")
+            value = col[index]
 
-    return "".join(out)
+            if [value.start, value.end][value.reversed] == line_no:
+                out.append("+")
+                horizontal = True
+                continue
 
+            if value.start <= line_no and line_no < value.end:
+                out.append("|" if not horizontal else "-")
+                continue
 
-class LineView:
-    def __init__(self, graph: Graph) -> None:
-        """
-        NOTE: Strings in graph should not have any newlines or other control characters
-        """
-        self.graph = graph
+            out.append(" " if not horizontal else "-")
 
-    def print(self, margin: int = 8) -> None:
-        slots: list[int | None] = [None] * margin
+        out.append("-<" if horizontal else "  ")
 
-        for id in range(self.graph.num_of_nodes()):
-            line = self.graph.nodes[id]
+        return "".join(out)
 
-            # jmp TO this instruction
-            ptr = 0
-            to_remove: list[int] = []
-            incoming = list(self.graph.adj_t[id])
-            indexes: list[int] = []
+    def display_incoming(self, line_no: int, line_width: int = 8) -> str:
+        line_width -= 2
+        out: list[str] = [" "] * (line_width - len(self.columns))
+        horizontal = False
 
-            for i in incoming:
-                if i < id:
-                    s = slots.index(id, ptr)
-                    indexes.append(s)
-                    to_remove.append(s)
-                    ptr += 1
-                else:
-                    s = slots.index(None)
-                    slots[s] = i
-                    indexes.append(s)
+        for col in self.columns[:line_width]:
+            # bisect_right gets line_no < x.start
+            # subtract 1 to get x.start <= line_no (our lower bound)
+            index = bisect_right(col, line_no, key=lambda x: x.start) - 1
 
-            if len(incoming) > 0:
-                print(format_slots(slots, ArrowEnum.Incoming, indexes), f"LABEL_{id}:")
+            if index < 0 or len(col) <= index:
+                out.append(" " if not horizontal else "-")
+                continue
 
-            for i in to_remove:
-                slots[i] = None
+            value = col[index]
 
-            # jmp FROM this instruction
-            ptr = 0
-            to_remove: list[int] = []
-            outgoing = list(self.graph.adj[id])
-            indexes: list[int] = []
+            if [value.end, value.start][value.reversed] == line_no:
+                out.append("+")
+                horizontal = True
+                continue
 
-            for i in outgoing:
-                if i < id:
-                    s = slots.index(id, ptr)
-                    indexes.append(s)
-                    to_remove.append(s)
-                    ptr += 1
-                else:
-                    s = slots.index(None)
-                    slots[s] = i
-                    indexes.append(s)
+            if value.start < line_no and line_no <= value.end:
+                out.append("|" if not horizontal else "-")
+                continue
 
-            if len(outgoing) > 0:
-                print(format_slots(slots, ArrowEnum.Outgoing, indexes), " ", line)
-            else:
-                print(format_slots(slots, ArrowEnum.Normal), " ", line)
+            out.append(" " if not horizontal else "-")
 
-            for i in to_remove:
-                slots[i] = None
+        out.append("->" if horizontal else "  ")
+        return "".join(out)
 
-        return
+    def print(self) -> None:
+        for line_no in range(len(self.lines)):
+            s = self.display_incoming(line_no)
+
+            if s[-1] != " ":
+                print(s, f"LINE_{line_no}:")
+
+            print(self.display_outgoing(line_no), " ", self.lines.nodes[line_no])
 
 
 def main():
@@ -151,23 +155,25 @@ add rax, 1
 mul rbx, 2
 jmp 0x3
 xor rcx, rcx
-xor rcx, rcx
+jmp 0x6
 xor rcx, rcx
 xor rcx, rcx
 xor rcx, rcx
 """.splitlines()
 
-    g = Graph()
+    g = Lines()
 
     for i in instructions:
-        g.add_node(i)
+        g.add_line(i)
 
     # TODO: add edges from CFG
-    g.add_edge(1, 4)
-    g.add_edge(4, 3)
-    g.add_edge(0, 8)
+    g.add_jump(1, 4)
+    g.add_jump(4, 3)
+    g.add_jump(6, 6)
+    g.add_jump(0, 8)
 
-    LineView(g).print()
+    m = Margin(g)
+    m.print()
 
 
 if __name__ == "__main__":
