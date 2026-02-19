dde1042f61be46f06314b148d09211ab1343bdc9
sammyuri <sammytrajtenberg@hotmail.com>
2026-02-19 11:36:32 +0000

    Added x86 call op

--- xdsl/dialects/x86_func.py
+++ xdsl/dialects/x86_func.py
@@ -3,31 +3,42 @@ from __future__ import annotations
 from collections.abc import Sequence
 
 from xdsl.backend.assembly_printer import AssemblyPrintable, AssemblyPrinter
-from xdsl.dialects.builtin import FunctionType, StringAttr, SymbolNameConstraint
+from xdsl.dialects.builtin import (
+    FunctionType,
+    StringAttr,
+    SymbolNameConstraint,
+    FlatSymbolRefAttrConstr,
+    SymbolRefAttr
+)
 from xdsl.dialects.utils import (
     parse_func_op_like,
     print_func_op_like,
 )
 from xdsl.dialects.x86.ops import X86Instruction
-from xdsl.ir import Attribute, Dialect, Operation, Region
+from xdsl.ir import Attribute, Dialect, Operation, Region, SSAValue
 from xdsl.irdl import (
     IRDLOperation,
-    attr_def,
     irdl_op_definition,
+    attr_def,
     opt_attr_def,
+    prop_def,
     region_def,
     traits_def,
+    var_operand_def,
+    var_result_def,
 )
 from xdsl.parser import Parser
 from xdsl.printer import Printer
 from xdsl.traits import (
+    SymbolUserOpInterface,
     CallableOpInterface,
     HasParent,
     IsolatedFromAbove,
     IsTerminator,
+    SymbolTable,
     SymbolOpInterface,
 )
-from xdsl.utils.exceptions import DiagnosticException
+from xdsl.utils.exceptions import DiagnosticException, VerifyException
 
 
 class FuncOpCallableInterface(CallableOpInterface):
@@ -45,6 +56,45 @@ class FuncOpCallableInterface(CallableOpInterface):
     def get_result_types(cls, op: Operation) -> tuple[Attribute, ...]:
         assert isinstance(op, FuncOp)
         return op.function_type.outputs.data
+
+        
+class CallOpSymbolUserOpInterface(SymbolUserOpInterface):
+    def verify(self, op: Operation) -> None:
+        assert isinstance(op, CallOp)
+
+        found_callee = SymbolTable.lookup_symbol(op, op.callee)
+        if not found_callee:
+            raise VerifyException(f"'{op.callee}' could not be found in symbol table")
+
+        if not isinstance(found_callee, FuncOp):
+            raise VerifyException(f"'{op.callee}' does not reference a valid function")
+
+        if len(found_callee.function_type.inputs) != len(op.arguments):
+            raise VerifyException("incorrect number of operands for callee")
+
+        if len(found_callee.function_type.outputs) != len(op.result_types):
+            raise VerifyException("incorrect number of results for callee")
+
+        for idx, (found_operand, operand) in enumerate(
+            zip(found_callee.function_type.inputs, (arg.type for arg in op.arguments))
+        ):
+            if found_operand != operand:
+                raise VerifyException(
+                    f"operand type mismatch: expected operand type {found_operand}, "
+                    f"but provided {operand} for operand number {idx}"
+                )
+
+        for idx, (found_res, res) in enumerate(
+            zip(found_callee.function_type.outputs, op.result_types)
+        ):
+            if found_res != res:
+                raise VerifyException(
+                    f"result type mismatch: expected result type {found_res}, but "
+                    f"provided {res} for result number {idx}"
+                )
+
+        return
+
 
 
 @irdl_op_definition
@@ -136,6 +186,42 @@ class FuncOp(IRDLOperation, AssemblyPrintable):
         printer.print_string(f"{self.sym_name.data}:\n")
 
 
+
+@irdl_op_definition
+class CallOp(X86Instruction):
+    """x86 function call operation"""
+
+    name = "x86_func.call"
+    arguments = var_operand_def()
+    callee = prop_def(FlatSymbolRefAttrConstr)
+    res = var_result_def()
+
+    traits = traits_def(
+        CallOpSymbolUserOpInterface(),
+    )
+
+    assembly_format = (
+        "$callee `(` $arguments `)` attr-dict `:` functional-type($arguments, $res)"
+    )
+
+    def __init__(
+        self,
+        callee: str | SymbolRefAttr,
+        arguments: Sequence[SSAValue | Operation],
+        return_types: Sequence[Attribute],
+    ):
+        if isinstance(callee, str):
+            callee = SymbolRefAttr(callee)
+        super().__init__(
+            operands=[arguments],
+            result_types=[return_types],
+            properties={"callee": callee},
+        )
+
+    def assembly_line_args(self):
+        return (self.callee.string_value(),)
+
+
 @irdl_op_definition
 class RetOp(X86Instruction):
     """
@@ -170,6 +256,7 @@ X86_FUNC = Dialect(
     "x86_func",
     [
         FuncOp,
+        CallOp,
         RetOp,
     ],
 )
