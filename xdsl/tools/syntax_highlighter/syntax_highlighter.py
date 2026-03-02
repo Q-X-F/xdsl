2a2aa32bd280208afc9fdc5ee95f9b7641a54535
Zoey <bee983868@gmail.com>
2026-03-02 19:28:48 +0000

    Update syntax_highlighter.py
    typo

--- xdsl/tools/syntax_highlighter/syntax_highlighter.py
+++ xdsl/tools/syntax_highlighter/syntax_highlighter.py
@@ -62,26 +62,6 @@ REGISTERS = [
     "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"
 ]
 
-ansi_colors = {
-    "black": "\033[30m",
-    "red": "\033[31m",
-    "green": "\033[32m",
-    "yellow": "\033[33m",
-    "blue": "\033[34m",
-    "magenta": "\033[35m",
-    "cyan": "\033[36m",
-    "white": "\033[37m",
-    "bright_black": "\033[90m",
-    "bright_red": "\033[91m",
-    "bright_green": "\033[92m",
-    "bright_yellow": "\033[93m",
-    "bright_blue": "\033[94m",
-    "bright_magenta": "\033[95m",
-    "bright_cyan": "\033[96m",
-    "bright_white": "\033[97m",
-    "reset": "\033[0m"
-}
-
 def highlight_x86(code: str) -> str:
     
     #Initialize config
