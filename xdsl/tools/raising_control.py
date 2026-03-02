6ece31e83f28bf159ba9ab6060d0519259672e2e
force-captain <72346798+force-captain@users.noreply.github.com>
2026-03-02 14:18:05 +0000

    Refactor if/else detection, add loop detection

--- xdsl/tools/raising_control.py
+++ xdsl/tools/raising_control.py
@@ -1,47 +1,94 @@
 from dataclasses import dataclass
-from xdsl.dialects.x86.ops import ConditionalJumpOperation
-from xdsl.ir.core import Block, Region
+
 from cfg import build_adj
+
+from xdsl.dialects.x86.ops import ConditionalJumpOperation
+from xdsl.ir import Block, Region
+
 
 @dataclass
 class ifElseBlock:
-    entry_Block: Block
+    entry_block: Block
     then_block: Block
     else_block: Block
     exit_block: Block
 
-def detect_if_blocks(region: Region) -> list[ifElseBlock]:
-    result: list[ifElseBlock] = []
 
+@dataclass
+class whileBlock:
+    entry_block: Block
+    body_block: Block
+    exit_block: Block
+
+
+def get_descendants(
+    block: Block,
+    adj_list: dict[Block, tuple[()] | tuple[Block] | tuple[Block, Block]],
+    visited: set[Block] = set(),
+) -> set[Block]:
+    if block in visited:
+        return {block}
+    visited.add(block)
+
+    children: tuple[()] | tuple[Block] | tuple[Block, Block] = adj_list[block]
+
+    result: set[Block] = set()
+
+    for child in children:
+        result |= get_descendants(child, adj_list, visited.copy())
+
+    return result | set(children)
+
+
+def detect_control_blocks(region: Region) -> list[ifElseBlock | whileBlock]:
+    result: list[ifElseBlock | whileBlock] = []
+
+    # Get blocks in order to determine merge points
+    block_order: dict[Block, int] = {b: i for i, b in enumerate(region.blocks)}
+
+    # dictionary from a block to its parent blocks
     parents: dict[Block, set[Block]] = {block: set() for block in region.blocks}
-    adj_list = build_adj(region)
 
-    # Populate parent dictionary
+    # get CFG representation and populate dict
+    adj_list: dict[Block, tuple[()] | tuple[Block] | tuple[Block, Block]] = build_adj(
+        region
+    )
+    descendants: dict[Block, set[Block]] = {
+        block: get_descendants(block, adj_list) for block in region.blocks
+    }
+
     for block, children in adj_list.items():
         for child in children:
             parents[child].add(block)
 
-    # Find potential entry blocks
+    # Find suitable entry blocks
     for entry, children in adj_list.items():
         lastop = entry.last_op
-
         if not isinstance(lastop, ConditionalJumpOperation):
             continue
 
-        then_block = lastop.then_block
-        else_block = lastop.else_block
-        
-        if then_block == else_block:
+        # Get children
+        bthen = lastop.then_block
+        belse = lastop.else_block
+
+        if bthen == belse:
             continue
 
-        fchild = adj_list[then_block]
-        schild = adj_list[else_block]
+        # Detect loop using ancestors
+        if entry in descendants[bthen]:
+            result.append(whileBlock(entry, bthen, belse))
+            continue
+        elif entry in descendants[belse]:
+            result.append(whileBlock(entry, belse, bthen))
+            continue
 
-        if len(fchild) != 1 or len(schild) != 1: continue
-        if fchild[0] is not schild[0]: continue
-        if len(parents[then_block]) != 1 or len(parents[else_block]) != 1: continue
-        if len(parents[fchild[0]]) != 1: continue
-        
-        result.append(ifElseBlock(entry, then_block, else_block, fchild[0]))
+        common_descendents = descendants[bthen] & descendants[belse]
+        if len(common_descendents) == 0:
+            continue
+        if len(parents[bthen]) != 1 or len(parents[belse]) != 1:
+            continue
 
+        exit_block = min(common_descendents, key=lambda b: block_order[b])
+
+        result.append(ifElseBlock(entry, bthen, belse, exit_block))
     return result
