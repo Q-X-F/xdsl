56cbd13ebd7cd504f2dd1f27dec975ed9d348a6f
Chris Qiu <qxfchris@gmail.com>
2026-03-03 12:56:30 +0000

    reverted disambiguation

--- xdsl/tools/lark_parser.py
+++ xdsl/tools/lark_parser.py
@@ -135,7 +135,7 @@ grammar = r"""
     mem : "[" REG (/[+-]/ OFFSET)? "]"
     COMMENT.3 : /;[^\n]*/
     %ignore COMMENT
-    OPCODE.2 : /(""" + ops_re + r""")[ \t\n\f\r]/
+    OPCODE.2 : /""" + ops_re + r"""/
     REG.2 : /""" + regs_re + r"""/
     %import common.SIGNED_NUMBER -> IMM
     %import common.NUMBER -> OFFSET
