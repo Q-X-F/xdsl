1e4c203091178d50fae8919c5246e5a55f8cb0bd
xhu6 <143360206+xhu6@users.noreply.github.com>
2026-03-03 14:53:58 +0000

    viewer: move functions into core

--- xdsl/viewer/command_line_tool.py
+++ xdsl/viewer/command_line_tool.py
@@ -1,20 +1,8 @@
 import argparse
 import locale
-import re
 import sys
-from io import StringIO
 
-from lark import ParseTree, Token, Tree
-
-from xdsl.ir import Region
-from xdsl.syntax_printer import SyntaxPrinter
-from xdsl.tools.convert_x86_to_mlir import X86Converter
-from xdsl.tools.lark_parser import parse
-from xdsl.tools.syntax_highlighter.syntax_highlighter import highlight_x86
-from xdsl.utils.colors import Colors
-from xdsl.viewer.core import LinearView, Lines
-
-# from xdsl.tools.cfg import group_functions
+from xdsl.viewer.core import LinearView, process_asm, process_mlir
 
 
 def supports_utf8() -> bool:
@@ -27,108 +15,6 @@ def is_safe() -> bool:
     Detects if safe to display Unicode and colors.
     """
     return sys.stdout.isatty() and supports_utf8()
-
-
-def convert_to_mlir(ast: ParseTree) -> Region:
-    converter = X86Converter()
-    res = converter.convert(ast)
-    return res
-
-
-def process_asm(text: str, color: bool) -> Lines:
-    lines = Lines()
-    tree = parse(text)
-
-    if color:
-        text = highlight_x86(text)
-
-    for line in text.split("\n"):
-        lines.add_line(line)
-
-    labels: dict[str, int] = {}
-    jumps: list[tuple[int, str]] = []
-
-    for t in tree.children:
-        if not isinstance(t, Tree):
-            raise ValueError
-
-        t2 = t.children[0]
-
-        if not isinstance(t2, Token):
-            raise ValueError
-
-        line_no = (t2.line or 0) - 1
-
-        if t.data == "label":
-            labels[str(t2)] = line_no
-
-        for operand in t.children[1:]:
-            if isinstance(operand, Token) and operand.type == "LABELNAME":
-                jumps.append((line_no, str(operand)))
-
-    for line_no, label in jumps:
-        lines.add_jump(line_no, labels[label])
-
-    return lines
-
-
-def process_mlir(text: str, color: bool) -> Lines:
-    lines = Lines()
-    tree = parse(text)
-    region = convert_to_mlir(tree)
-
-    s = StringIO()
-    SyntaxPrinter(s).print_region(region)
-
-    block_line_nos: list[int] = []
-    block_names: list[str] = []
-
-    indents: list[int] = []
-
-    for line in s.getvalue().split("\n"):
-        line = line.lstrip()
-        if line.startswith("^bb") or line.startswith("x86_func"):
-            block_names.append(line.split("(")[0].lstrip())
-
-            if len(block_line_nos) > 0:
-                block_line_nos.append(len(lines) - 1)
-                lines.add_line("")
-                lines.add_line("")
-                lines.add_line("")
-            block_line_nos.append(len(lines))
-
-        lines.add_line("    " * len(indents[1:]) + line)
-
-        if "}" in line:
-            last = indents.pop()
-
-            if last == 0:
-                continue
-
-            lines.add_jump(last, len(lines) - 1, Colors.RED)
-
-        if "{" in line:
-            indents.append(len(lines) - 1)
-
-    if len(block_line_nos) > 0:
-        block_line_nos.append(len(lines) - 1)
-
-        lines.add_line("")
-        lines.add_line("")
-        lines.add_line("")
-
-    for pos in block_line_nos[1::2]:
-        line = lines.lines[pos]
-        bbs = re.findall("(\\^bb\\d+)", line)
-
-        if len(bbs) == 0:
-            continue
-
-        bb = bbs[0]
-        color2 = Colors.BLUE if "fallthrough" not in line else Colors.WHITE
-        lines.add_jump(pos, block_line_nos[2 * block_names.index(bb)], color2)
-
-    return lines
 
 
 def main():
